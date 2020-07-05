3d086f5c68d57fc5a1a23e9fcfdbf88852f21707
PixelDotPixel <spinks@oregonstate.edu>
2020-07-05 13:42:21 -0700

    Initial commit of navigation drawer and reformated files

--- scello/lib/main.dart
+++ scello/lib/main.dart
@@ -1,153 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:scello/API_Module/Networking.dart';
-import 'package:scello/Data/NutritionOBJ.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
+import 'package:scello/Navigator/routeGenerator.dart';
+import 'package:scello/Navigator/NavigatorDrawer.dart';
+
+void main() => runApp(SCELLO());
+
+class SCELLO extends StatelessWidget {
+    @override
+    Widget build(BuildContext context){
+        return MaterialApp(
+            initialRoute: '/',
+            onGenerateRoute: RouteGenerator.generateRoute,
+        );
+    }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class HomePage extends StatefulWidget {
+    HomePage();
+
+    @override
+    _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _HomePageState extends State<HomePage> {
+    final routeName = '/';
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final myController = TextEditingController();
-  NutritionOBJ nutritionOBJ;
-
-  void _apiCall() async {
-    String term;
-    networkingAPI networking = new networkingAPI();
-    nutritionOBJ = await networking.controller(myController.text);
-    setState(() {
-
-    });
-  }
-
-  List<Widget> getNewBody(NutritionOBJ nutritionOBJ){
-    String toPrint = nutritionOBJ.foodName;
-    List<Widget> bodyToReturn = [];
-
-    Widget search = new TextField(
-        controller: myController,
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            labelText: 'Search a food term'
-        )
-    );
-
-    bodyToReturn.add(search);
-
-    Widget text = new Text(
-        toPrint,
-        textAlign: TextAlign.center,
-        style: TextStyle(fontWeight: FontWeight.bold)
-    );
-    bodyToReturn.add(text);
-
-    for(int i = 0; i < nutritionOBJ.totalNutriants.categories.length; i++){
-      if(nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]] != null){
-        String nutritionalTextFormatted = nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].label
-            + " : "
-            + nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].quantity.toString()
-            + " "
-            + nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].unit;
-
-        Widget nutritionTile = new Container(
-            child: new Card(
-              child: new Material(
-                child: new InkWell(
-                  //onTap: () => _onTileClicked(),
-                  //onLongPress: () => _onTileLongClicked(),
-                  splashColor: Colors.grey,
-                  child: Center (
-                    child: Text(
-                        nutritionalTextFormatted,
-                        textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              )
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            drawer: Drawer(
+                child: NavigatorDrawer(routeName)
             ),
-          );
-        bodyToReturn.add(nutritionTile);
-
-        //        toPrint = toPrint + "\n"
-//            + nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].label
-//            + " : "
-//            + nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].quantity.toString()
-//            + nutritionOBJ.totalNutriants.categoriesOBJS[nutritionOBJ.totalNutriants.categories[i]].unit;
-      }
+            appBar: AppBar(
+                title: Text("Scello"),
+            ),
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        Text("Scello")
+                    ],
+                ),
+            ),
+        );
     }
-    
-    return bodyToReturn;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if(nutritionOBJ != null){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: ListView(
-            children: getNewBody(nutritionOBJ),
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _apiCall,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                  controller: myController,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: 'Search a food term'
-                  )
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _apiCall,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      );
-    }
-  }
 }
